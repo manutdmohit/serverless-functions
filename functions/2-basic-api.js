91a642a9794eee3f575ed86641fed712963d52ed
manutdmohit <saudmohit@gmail.com>
2021-09-25 07:27:01 +0545

    added headers removed cors policy

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,7 +1,10 @@
-const items = require("../assets/data")
+const items = require('../assets/data');
 
 exports.handler = async (event, context, cb) => {
   return {
+    headers: {
+      'Access-Control-Allow-origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
